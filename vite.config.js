28d3ef22efa3ac1f886502201f7cdc65da6ab7fc
KDPersonalTraining <kyle.kdpt@gmail.com>
2026-07-05 17:27:02 +0100

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          icons: ['lucide-react'],
-        },
-      },
-    },
   },
 })
